7a9eaad384793a41fbd612f042571a35b1ae328d
zehrainoz <134527110+zehrainoz@users.noreply.github.com>
2025-09-02 00:13:16 +0300

    Add functionality to return maximum sum increasing subarray as JSON

--- MaxIncreasingSubArrayAsJson.cs
+++ MaxIncreasingSubArrayAsJson.cs
@@ -1,51 +1,46 @@
-using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.Text.Json;
 
-    public static void MaxIncreasingSubArrayAsJson(List<int> numbers)
+    public static string MaxIncreasingSubArrayAsJson(List<int> numbers)
     {
-
         if (numbers.Count == 0)
         {
-            return;
+            return "[]";
         }
 
         List<List<int>> increasingSubArrays = new List<List<int>>();
-        List<int> currentSublist = new List<int>();
+        List<int> currentSubArray = new List<int>();
 
         //Find and save increasing subArrays
         for (int i = 0; i < numbers.Count; i++)
         {
-            if (currentSublist.Count == 0)
+            if (currentSubArray.Count == 0)
             {
-                currentSublist.Add(numbers[i]);
+                currentSubArray.Add(numbers[i]);
             }
 
-            else if (numbers[i] > currentSublist[currentSublist.Count - 1])
+            //Add current element if greater than the last element in the subArray
+            else if (numbers[i] > currentSubArray[currentSubArray.Count - 1])
             {
-                currentSublist.Add(numbers[i]);
+                currentSubArray.Add(numbers[i]);
             }
 
+            //Add current subArray when the sequence is broken
             else
             {
-                increasingSubArrays.Add(new List<int>(currentSublist));
-                currentSublist = new List<int> { numbers[i] };
+                increasingSubArrays.Add(new List<int>(currentSubArray));
+                currentSubArray = new List<int> { numbers[i] };
             }
         }
 
+        //Add the last subarray because it wasn't added yet
+        increasingSubArrays.Add(new List<int>(currentSubArray));
 
-        increasingSubArrays.Add(new List<int>(currentSublist));
+        //Save maximum increasing subArray
+        var maxIncreasingSubArray = increasingSubArrays.OrderByDescending(sub => sub.Sum()).First();
 
-        var maxSubarray = increasingSubArrays.OrderByDescending(sub => sub.Sum()).First();
-
-        
-        // Test
-        foreach (var max in maxSubarray)
-        {
-            Console.WriteLine(max);
-        }
-        
-
+        return JsonSerializer.Serialize(maxIncreasingSubArray);
     }
 
 
