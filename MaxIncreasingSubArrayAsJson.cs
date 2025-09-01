df006dad73bdd9685542397589dd51a4d9fd0335
zehrainoz <134527110+zehrainoz@users.noreply.github.com>
2025-09-01 23:15:22 +0300

    Add initial implementation to find increasing subarrays

---
+++ MaxIncreasingSubArrayAsJson.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+
+
+
+    public static void MaxIncreasingSubArrayAsJson(List<int> numbers)
+    {
+
+        if (numbers.Count == 0)
+        {
+            return;
+        }
+
+        List<List<int>> increasingSubArrays = new List<List<int>>();
+        List<int> currentSublist = new List<int>();
+
+        //Find and save increasing subArrays
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            if (currentSublist.Count == 0)
+            {
+                currentSublist.Add(numbers[i]);
+            }
+
+            else if (numbers[i] > currentSublist[currentSublist.Count - 1])
+            {
+                currentSublist.Add(numbers[i]);
+            }
+
+            else
+            {
+                increasingSubArrays.Add(new List<int>(currentSublist));
+                currentSublist = new List<int> { numbers[i] };
+            }
+        }
+    
+
+        increasingSubArrays.Add(new List<int>(currentSublist));
+
+        foreach (var sub in increasingSubArrays)
+        {
+            Console.WriteLine($"[{string.Join(", ", sub)}]");
+        }
+
+    }
+
+
